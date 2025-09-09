b1faab8cfc3adc15e46b7d7d8106b2c9a2a3b502
Seleya12 <Seleya-Katharina.Koch@stud.leuphana.de>
2025-09-09 20:44:33 +0200

    adding a start screen (title + instructions)

--- actual-code.py
+++ actual-code.py
@@ -16,3 +16,14 @@ BIG_R = 38
 PLAY_TIME = 20.0
 
 LOOP_PATH = os.path.join("media", "loop.ogg") #path for the possible sound
+
+
+def draw_start(screen, big, small): 
+    screen.fill(BG)
+    title = big.render("ShyRhythm", True, WHITE)
+    hint = small.render("Press ENTER to start", True, WHITE)
+    screen.blit(title, (WIDTH//2 - title.get_width()//2, HEIGHT//2 - 40))
+    screen.blit(hint, (WIDTH//2 - hint.get_width()//2, HEIGHT//2 + 10))
+    if audio_loaded: 
+        msg = small.render("Press M to toggle music", True, WHITE)
+        screen.blit(msg, (WIDTH//2 - msg.get_width()//2, HEIGHT//2 + 40)
